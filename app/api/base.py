99096f438b0e86ac0376182b4ff3957d4d75a5d0
Lucas Paixão <lucas.lsop@gmail.com>
2023-12-16 20:21:40 -0300

    Integração com MongoDB

--- app/api/base.py
+++ app/api/base.py
@@ -9,3 +9,44 @@ def fetch_collection(db, col, filter={}):
     for doc in docs:
         doc['_id'] = str(doc['_id'])
     return docs
+
+
+def extend_doc(doc):
+    # Variedade do produto
+    if doc['marca'] is None:
+        a = doc['produto']
+    else:
+        a = doc['marca']
+
+    if doc['quantidade'].is_integer():
+        b = int(doc['quantidade'])
+    else:
+        b = doc['quantidade']
+
+    c = doc['unidade']
+
+    doc['variedade'] = f'{a} {b} {c}'
+
+    # Preço de referência
+    if doc['unidade'] == 'UN':
+        factor = 10
+        unit = '10 un'
+    elif doc['unidade'] == 'KG':
+        factor = 1
+        unit = 'kg'
+    elif doc['unidade'] == 'G':
+        factor = 1e3
+        unit = 'kg'
+    elif doc['unidade'] == 'L':
+        factor = 1
+        unit = 'L'
+    elif doc['unidade'] == 'ML':
+        factor = 1e3
+        unit = 'L'
+    else:
+        factor = 0
+        unit = None
+    doc['preco_ref'] = doc['preco']*factor/doc['quantidade']
+    doc ['unidade_ref'] = unit
+
+    return doc
--- app/api/views.py
+++ app/api/views.py
@@ -1,16 +1,11 @@
 import pandas as pd
 
-from .base import db, fetch_collection
+from .base import db, extend_doc, fetch_collection
 
 
-def eval_unit_price(item):
-    factor = 1
-    item['tam_ref'] = item['un']
-    if item['un'] == 'g':
-        factor = 1e-3# 1 g = 10^-3 kg
-        item['tam_ref'] = 'kg'
-    item['preco_un'] = item['preco']/(item['tam_emb']*factor)
-    return item
+def get_precos(produto):
+    docs = fetch_collection(db, 'preco', filter={'produto': produto})
+    return [ extend_doc(doc) for doc in docs ]
 
 
 def get_lojas():
@@ -19,12 +14,6 @@ def get_lojas():
 
 def get_marcas(produto):
     return fetch_collection(db, 'marca')
-
-
-def get_precos(produto):
-    docs = fetch_collection(db, 'preco', filter={'produto': produto})
-    docs = [ eval_unit_price(doc) for doc in docs if doc['produto'] == produto ]
-    return docs
 
 
 def get_produtos():
